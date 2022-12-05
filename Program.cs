5e595d0f9f62f92c3efff73672d5fc420092de24
Stefano Savarino <stefano@reachconsulting.se>
2022-12-05 21:36:10 +0100

    First half of day1 completed. Highest carrying elf found.

--- Program.cs
+++ Program.cs
@@ -10,14 +10,14 @@ class Day1
     class Elf
     {
         private int id = 0;
-        private ArrayList food_calories;
+        private List<int> food_calories;
         private int total_calories = 0;
 
         //Empty constructor
         public Elf()
         {
             this.id = 0;
-            this.food_calories = new ArrayList();
+            this.food_calories = new List<int>();
             this.total_calories = 0;
         }
 
@@ -25,33 +25,55 @@ class Day1
         public Elf(int id)
         {
             this.id = id;
-            this.food_calories = new ArrayList();
+            this.food_calories = new List<int>();
             this.total_calories = 0;
+        }
+        
+        public void AddItemCalories(int item_calories)
+        {
+            this.food_calories.Add(item_calories);
+            this.total_calories += item_calories;
+        }
+        
+        public int GetTotalCalories()
+        {
+            return this.total_calories;
         }
     }
 
     static void Main()
     {
         string path = @"C:\Users\StefanoSavarino\source\repos\AdventOfCode\2022-12-01 input\2022-12-01 input";
-        ArrayList elfs = new ArrayList();
+        List<Elf> elfs = new List<Elf>();
 
         int counter = 0;
+        int highest_calories = 0;
         elfs.Add(new Elf(counter));
         
         foreach(string line in System.IO.File.ReadLines(path))
         {
             if (line == "")
             {
+                Console.WriteLine("Old Elf total calories: " + elfs[counter].GetTotalCalories());
                 counter++;
                 elfs.Add(new Elf(counter));
                 Console.WriteLine("counter = " + counter);
             }
             else
             {
-                
+                elfs[counter].AddItemCalories(Int32.Parse(line));
                 Console.WriteLine(line);
             }
         }
-            
+        
+        foreach(Elf el in elfs)
+        {
+            if(el.GetTotalCalories() > highest_calories)
+                highest_calories = el.GetTotalCalories();
+            Console.WriteLine("Current highest calories count: " + highest_calories);
+        }
+
+        Console.WriteLine("highest calories = " + highest_calories);
+
     }
 }
